b29f198c94606d15c3463c82051bf0c62e6b9261
Leonardo Montini <lion.48m@gmail.com>
2023-06-28 21:54:59 +0200

    Add User-Agent header to get access after the new chess.com api policies

--- src/fetch.ts
+++ src/fetch.ts
@@ -2,12 +2,22 @@ import http from 'https';
 
 export default function fetch<Type>(url: string): Promise<Type> {
   return new Promise<Type>((res, rej) => {
-    http.get(url, response => {
-      let data = '';
+    http.get(
+      {
+        host: 'api.chess.com',
+        path: url,
+        headers: {
+          'Content-Type': 'application/json',
+          'User-Agent': 'chesscom-stats-github-action'
+        }
+      },
+      response => {
+        let data = '';
 
-      response.on('data', chunk => (data += chunk));
-      response.on('end', () => res(JSON.parse(data)));
-      response.on('error', e => rej(e));
-    });
+        response.on('data', chunk => (data += chunk));
+        response.on('end', () => res(JSON.parse(data)));
+        response.on('error', e => rej(e));
+      }
+    );
   });
 }
